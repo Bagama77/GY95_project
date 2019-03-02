674f517deefa1b8986bae17d02fade821006ccf4
Bagama <szamikx@gmail.com>
2019-03-03 00:02:37 +0200

    format showing results to console

--- src/Main.java
+++ src/Main.java
@@ -44,8 +44,8 @@ public class Main {
             float rY = itg3205.getR()[1];
             float rZ = itg3205.getR()[2];
             //--------------print in console-----------------------
-            System.out.printf("ADXL: X-Axis = %d, Y-Axis = %d, Y-Axis = %d ", xAccl, yAccl, zAccl);
-            System.out.printf("ITG3205: x= %f, y=%f, z=%f deg/s", rX, rY, rZ);
+            System.out.printf("ADXL: X-Axis = %d, Y-Axis = %d, Y-Axis = %d /r/n", xAccl, yAccl, zAccl);
+            System.out.printf("ITG3205: x= %f, y=%f, z=%f deg/s /r/n", rX, rY, rZ);
             Thread.sleep(500);
         }
 
