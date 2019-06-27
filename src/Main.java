c4aad59d5755548e584bfdf4983d6dda378faf3b
Bagama <szamikx@gmail.com>
2019-06-27 23:47:25 +0300

    new build9

--- src/Main.java
+++ src/Main.java
@@ -45,9 +45,9 @@ public class Main {
             float rY = itg3205.getR()[1];
             float rZ = itg3205.getR()[2];
             //--------------print in console-----------------------
-            System.out.printf("ADXL: X-Axis = %d, Y-Axis = %d, Z-Axis = %d \r\n", xAccl, yAccl, zAccl);
-            System.out.printf("ITG3205: x= %f, y=%f, z=%f deg/s \r\n", rX, rY, rZ);
-            Thread.sleep(500);
+//            System.out.printf("ADXL: X-Axis = %d, Y-Axis = %d, Z-Axis = %d \r\n", xAccl, yAccl, zAccl);
+//            System.out.printf("ITG3205: x= %f, y=%f, z=%f deg/s \r\n", rX, rY, rZ);
+            Thread.sleep(50);
 
             //calculation of angle_filtered (complementary filter)
 
